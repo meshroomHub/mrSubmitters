7b507d39693eb495bd65d15c47521b76ed9ba3bc
Alice Sonolet <alice.sonolet@gmail.com>
2026-06-22 17:41:18 +0200

    Implement multi command and use the CommandArgsBuilder

--- meshroom/tractorSubmitter/api/subtaskCreator.py
+++ meshroom/tractorSubmitter/api/subtaskCreator.py
@@ -82,13 +82,13 @@ def sendTractorCmd(task_def):
             f.write("\n" + task_def + "\n")
 
 
-def queueSubtask(title, argv, service="", limits=None, metadata=None, envkey=None):
+def queueSubtask(commands, title, service="", limits=None, metadata=None, envkey=None):
     """
     Queue a subtask to be created in Tractor.
 
     Args:
+        commands (list): Commands to run
         title (str): Task title
-        cmd (str or list): Command to run (string or argv list)
         service (str): Tractor service key
         limits (list): Limit tags (e.g. ["blender", "nuke"])
         metadata (dict): Metadata as key:value pairs
@@ -98,21 +98,13 @@ def queueSubtask(title, argv, service="", limits=None, metadata=None, envkey=Non
 
     Example:
         queueSubtask(
-            title="render_frame_0001",
             cmd="render --frame 1 scene.ma",
+            title="render_frame_0001",
             service="mikrosRender",
             limits=["blender"],
             metadata={'user': 'john', 'iteration': '1', 'prod': 'mvg'}
         )
     """
-
-    # Parse command
-    if isinstance(argv, str):
-        cmd_argv = shlex.split(argv)
-    else:
-        cmd_argv = list(argv)
-
-    cmd_str = " ".join(cmd_argv)
 
     # Build tags string
     tags_str = ""
@@ -131,12 +123,21 @@ def queueSubtask(title, argv, service="", limits=None, metadata=None, envkey=Non
 
     # Build service string
     service_str = f"-service {{{service}}}" if service else ""
+    
+    # Build list of commands
+    tclCmds = []
+    for argv in commands:
+        cmd_str = " ".join(argv)
+        tclCmds.append(f"""RemoteCmd {{{cmd_str}}} {service_str} {tags_str} {envkey_str}""")
 
     # Write Alfred task definition
     # TODO : we can use tractor API to convert a Task into alf (asTcl)
     task_def = f"""
-Task -title {{{title}}} {service_str} {metadata_str} -cmds {{
-    RemoteCmd {{{cmd_str}}} {service_str} {tags_str} {envkey_str}
+Task -title {{{title}}} {service_str} {metadata_str} -cmds {{"""
+    for cmd in tclCmds:
+        task_def += f"""
+    {cmd}"""
+    task_def += f"""
 }}
 """
     print(task_def)
@@ -144,7 +145,7 @@ Task -title {{{title}}} {service_str} {metadata_str} -cmds {{
     log(f"Queued subtask: {title}")
 
 
-def queueChunkTask(node, cmdArgs, service, tags=None, reqPackages=None, environment=None):
+def queueChunkTask(node, taskCommand, config, tags=None, reqPackages=None, environment=None):
     blockSize, fullSize, nbBlocks = node.nodeDesc.parallelization.getSizes(node)
     if nbBlocks <= 0:
         return
@@ -152,19 +153,21 @@ def queueChunkTask(node, cmdArgs, service, tags=None, reqPackages=None, environm
     
     for iteration in range(nbBlocks):
         taskInfo = TaskInfo(
-            name=node.name, 
-            cmdArgs=cmdArgs,
-            nodeUid=node._uid,
+            name=node.name,
+            node=node, 
+            cmdArgs=taskCommand,
             environment=environment,
             reqPackages=reqPackages,
-            service=service,
+            config=config,
             licenses=licenses,
             taskType=("chunk", iteration),
             tags=tags.copy() if tags else None,
         )
         # title, argv, service, metadata
-        taskArgs = taskInfo.cook()
+        taskArgs = taskInfo.get_kwargs()
         # limits, envkey
         taskArgs['limits'] = taskInfo.limits
         taskArgs['envkey'] = taskInfo.envkey
-        queueSubtask(**taskArgs)
+        # Commands
+        commands = taskInfo.get_commands()
+        queueSubtask(commands, **taskArgs)
